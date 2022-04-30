7138c8b5af1d30281ff4b3fa7944a2ef4c444fe7
tamal3472 <44064679+tamal3472@users.noreply.github.com>
2022-04-30 16:10:32 +0600

    restructure campaign spec

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -8,17 +8,15 @@ RSpec.describe Campaign, type: :model do
   it { is_expected.to validate_presence_of :investment_multiple }
 
   describe 'image upload' do
-    describe 'Upload avatar' do
-      context 'with a valid image' do
-        let(:campaign_one) { FactoryBot.create(:campaign) }
-        it 'saves the image' do
-          campaign_one.image.attach(
-            io: File.open('spec/fixtures/images/rails.png'),
-            filename: 'rails_image',
-            content_type: 'image/png',
-          )
-          expect(campaign_one.reload.image).to be_attached
-        end
+    context 'with a valid image' do
+      let(:campaign) { FactoryBot.create(:campaign) }
+      it 'saves the image' do
+        campaign.image.attach(
+          io: File.open('spec/fixtures/images/rails.png'),
+          filename: 'rails_image',
+          content_type: 'image/png',
+        )
+        expect(campaign.reload.image).to be_attached
       end
     end
   end
