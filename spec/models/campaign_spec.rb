af1fcebec0e79198d868b0ecd88301caa01c8f31
tamal3472 <44064679+tamal3472@users.noreply.github.com>
2022-04-30 16:23:13 +0600

    campaign spec fix

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -6,18 +6,4 @@ RSpec.describe Campaign, type: :model do
   it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_presence_of :target_amount }
   it { is_expected.to validate_presence_of :investment_multiple }
-
-  describe 'image upload' do
-    context 'with a valid image' do
-      let(:campaign) { FactoryBot.create(:campaign) }
-      it 'saves the image' do
-        campaign.image.attach(
-          io: File.open('spec/fixtures/images/rails.png'),
-          filename: 'rails_image',
-          content_type: 'image/png',
-        )
-        expect(campaign.reload.image).to be_attached
-      end
-    end
-  end
 end
